3a0b41d3e8ae81dcc03b2caf525b85fc278b22d0
nbogie <neillbogie@googlemail.com>
2022-05-11 17:10:46 +0100

    tweak helpers

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ import { setupLights } from './setupLights';
 import { setupOrbitControls } from './setupOrbitControls';
 import { setupRenderer } from './setupRenderer';
 function setupHelpers(scene: Scene) {
-    const axesHelper = new AxesHelper(5);
+    const axesHelper = new AxesHelper(10);
     scene.add(axesHelper);
-    const gridHelper = new GridHelper(5);
+    const gridHelper = new GridHelper(100);
     scene.add(gridHelper);
 }
 export function setupThreeJSScene() {

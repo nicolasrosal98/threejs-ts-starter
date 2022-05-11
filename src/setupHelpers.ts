41d3264b619c9b64493a12c859d6c0b70e22b5f5
nbogie <neillbogie@googlemail.com>
2022-05-11 18:33:11 +0100

    move axeshelper out of cluster

--- src/setupHelpers.ts
+++ src/setupHelpers.ts
@@ -2,7 +2,7 @@ import { Scene, AxesHelper, GridHelper } from "three";
 
 export function setupHelpers(scene: Scene) {
     const axesHelper = new AxesHelper(10);
-    axesHelper.position.set(0, 1, 0); //lift up from grid
+    axesHelper.position.set(-50, 25, 0); //lift up from grid
     scene.add(axesHelper);
     const gridHelper = new GridHelper(100);
     scene.add(gridHelper);

3c1c6159c790b0f7039d3af705ba3383484ff884
nbogie <neillbogie@googlemail.com>
2022-05-12 23:05:27 +0100

    add doc about installing @types/w3c-generic-sensor

--- src/setupAccelerometer.ts
+++ src/setupAccelerometer.ts
@@ -1,3 +1,5 @@
+//It seems we need to install @types/w3c-generic-sensor
+
 export function setupAccelerometer(callbackFn: (acl: Accelerometer) => void) {
     let acl = new Accelerometer({ frequency: 60 });
     acl.addEventListener('reading', () => callbackFn(acl));

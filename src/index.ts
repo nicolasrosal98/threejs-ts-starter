0247dba2ae668f63794dacb0c3174c66162d780d
nbogie <neillbogie@googlemail.com>
2022-05-13 01:54:06 +0100

    move model loading out to own file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { Scene } from 'three';
-import { loadModel } from './loadModel';
 import { setupCamera } from './setupCamera';
 import { setupHelpers } from './setupHelpers';
 import { setupLights } from './setupLights';
 import { setupOrbitControls } from './setupOrbitControls';
 import { setupRenderer } from './setupRenderer';
+import { setupTreesAndHouseModel } from './setupTreesAndHouseModel';
 
 export async function setupThreeJSScene() {
 
@@ -22,18 +22,10 @@ export async function setupThreeJSScene() {
 
     setupHelpers(scene);
 
-
-    const treesAndHouse = await loadModel("./assets/treesAndHouse.glb");
+    const treesAndHouse = await setupTreesAndHouseModel(scene);
     if (treesAndHouse) {
-        treesAndHouse.scale.set(5, 5, 5);
-        treesAndHouse.position.setX(50);
-        scene.add(treesAndHouse);
+        treesAndHouse.position.setX(-30);
     }
-
-    //You can get more models from https://market.pmnd.rs/
-
-    //keep a frame counter so we can use it as an input to an animation
-    let frameCount = 0;
 
     animate();
 
@@ -42,9 +34,7 @@ export async function setupThreeJSScene() {
 
         controls.update(); // required if controls has .enableDamping .autoRotate set true.
 
-        // document.getElementById("info")!.innerText = "z: " + Math.round(submarine!.position.z);
         requestAnimationFrame(animate);
-        frameCount++;
     }
 }
 
---
+++ src/setupTreesAndHouseModel.ts
@@ -0,0 +1,15 @@
+import { Group, Scene } from 'three';
+import { loadModel } from './loadModel';
+
+export async function setupTreesAndHouseModel(scene: Scene): Promise<Group | null> {
+
+    const treesAndHouse = await loadModel("./assets/treesAndHouse.glb");
+    if (treesAndHouse) {
+        treesAndHouse.scale.set(5, 5, 5);
+
+        scene.add(treesAndHouse);
+        return treesAndHouse;
+    }
+    return null;
+
+}

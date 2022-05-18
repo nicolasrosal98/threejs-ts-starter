eb0cc8753b1627492937baf0ce073d59dfcd1113
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-05-18 05:41:53 -0500

    Add christmas tree

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,60 @@
-import {
-    Scene,
-    Mesh,
-    MeshStandardMaterial,
-    BoxBufferGeometry,
-} from 'three';
-import { setupCamera } from './setupCamera';
-import { setupHelpers } from './setupHelpers';
-import { setupLights } from './setupLights';
-import { setupOrbitControls } from './setupOrbitControls';
-import { setupRenderer } from './setupRenderer';
+import { Scene, Mesh, MeshStandardMaterial, BoxBufferGeometry, ConeGeometry } from "three";
+import { setupCamera } from "./setupCamera";
+import { setupHelpers } from "./setupHelpers";
+import { setupLights } from "./setupLights";
+import { setupOrbitControls } from "./setupOrbitControls";
+import { setupRenderer } from "./setupRenderer";
 
 export function setupThreeJSScene() {
+  const dimensions = { w: window.innerWidth, h: window.innerHeight };
 
-    let dimensions = { w: window.innerWidth, h: window.innerHeight };
+  const camera = setupCamera(dimensions);
 
-    const camera = setupCamera(dimensions);
+  const renderer = setupRenderer(camera, dimensions);
 
-    const renderer = setupRenderer(camera, dimensions);
+  const controls = setupOrbitControls(camera, renderer.domElement);
 
-    const controls = setupOrbitControls(camera, renderer.domElement);
+  const scene = new Scene();
 
-    let scene = new Scene();
+  setupLights(scene);
 
-    setupLights(scene);
+  setupHelpers(scene);
 
-    setupHelpers(scene);
+  //shape(s)
+  //// Cube
+  const cubeGeometry = new BoxBufferGeometry(10, 10, 10);
+  const cubeMaterial = new MeshStandardMaterial({
+    color: 0xff00ff,
+  });
+  const myCube: Mesh = new Mesh(cubeGeometry, cubeMaterial);
+  myCube.position.y = 20;
+  scene.add(myCube);
+  ////Christmas tree
+  const coneGeometry = new ConeGeometry(2, 5, 10);
+  const coneMaterial = new MeshStandardMaterial({
+    color: 0x00ff00
+  });
+  const myLowerTree: Mesh = new Mesh(coneGeometry, coneMaterial);
+  const myUpperTree: Mesh = new Mesh(coneGeometry, coneMaterial);
+  myLowerTree.position.set(50,5,-50)
+  myUpperTree.position.set(50,8,-50)
+  scene.add(myLowerTree)
+  scene.add(myUpperTree)
+  
 
-    //shape(s)
-    const geometry = new BoxBufferGeometry(10, 10, 10);
-    const material = new MeshStandardMaterial({
-        color: 0xff00ff
-    });
+  animate();
 
-    let myShape: Mesh = new Mesh(geometry, material);
-    myShape.position.y = 20;
-    scene.add(myShape);
+  function animate() {
+    myCube.rotation.y += 0.01;
+    myCube.rotation.x += 0.02;
 
+    renderer.render(scene, camera);
 
-    animate();
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
 
-
-    function animate() {
-        myShape.rotation.y += 0.01;
-        myShape.rotation.x += 0.02;
-
-        renderer.render(scene, camera);
-
-        // required if controls.enableDamping or controls.autoRotate are set to true
-        controls.update();
-
-        requestAnimationFrame(animate);
-    }
+    requestAnimationFrame(animate);
+  }
 }
 
 setupThreeJSScene();

fc45cf67f0cc47311ce581c86fcf72ca13cb5053
nbogie <neillbogie@googlemail.com>
2022-05-31 00:21:47 +0100

    add setupPolarGridHelper

---
+++ src/setupPolarGridHelper.ts
@@ -0,0 +1,11 @@
+import { PolarGridHelper, Scene } from "three";
+
+export function setupPolarGridHelper(scene: Scene) {
+    const radius = 50;
+    const radials = 16;
+    const circles = 8;
+    const divisions = 64;
+
+    const helper = new PolarGridHelper(radius, radials, circles, divisions);
+    scene.add(helper);
+}

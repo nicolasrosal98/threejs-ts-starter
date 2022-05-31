968d8c17c8d18fe2a06093211f56146d6a2f7292
nbogie <neillbogie@googlemail.com>
2022-05-31 22:13:54 +0100

    placate eslint

--- src/setupPolarGridHelper.ts
+++ src/setupPolarGridHelper.ts
@@ -1,6 +1,6 @@
 import { PolarGridHelper, Scene } from "three";
 
-export function setupPolarGridHelper(scene: Scene) {
+export function setupPolarGridHelper(scene: Scene): void {
     const radius = 50;
     const radials = 16;
     const circles = 8;

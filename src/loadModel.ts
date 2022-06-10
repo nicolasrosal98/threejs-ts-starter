a6b2bf772466d93a311e5ef39e97a0f4e820e2d5
nbogie <neillbogie@googlemail.com>
2022-06-10 21:54:41 +0100

    add link to how decoder paths are set up in gltf-viewer

--- src/loadModel.ts
+++ src/loadModel.ts
@@ -9,8 +9,10 @@ export async function loadModel(url: string): Promise<Group | null> {
 
     // Optional: Provide a DRACOLoader instance to decode compressed mesh data - only some gltf files need this.
     const dracoLoader = new DRACOLoader();
-    //From a CDN.  We ought instead to take this from the local installed modules, but with parcel bundler, that's complex.
-    //Note this is a specific version.
+    // From a CDN.  We ought instead to take this from the local installed modules, but with parcel bundler, that's complex.
+    // Note this is a specific version.
+    // See also how don mccurdy does this in gltf-viewer 
+    // https://github.com/donmccurdy/three-gltf-viewer/blob/890e4bc84d203058a6f2673e2b6f3ff19b6a3e5b/src/viewer.js#L38    
     dracoLoader.setDecoderPath('https://www.gstatic.com/draco/versioned/decoders/1.5.2/');
     loader.setDRACOLoader(dracoLoader);
 

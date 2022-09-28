b7826f6a87ab0441684e4f48668b0827f03c21e9
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-09-28 15:49:00 +0100

    Re-order @params to show camera first

--- src/setupOrbitControls.ts
+++ src/setupOrbitControls.ts
@@ -1,20 +1,21 @@
 import { Camera } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-
-/** 
- * @param htmlElement - html (canvas?) element on which to listen for mouse events 
+/**
  * @param camera - camera to move, rotate and zoom based on user input
-*/
-export function setupOrbitControls(camera: Camera, htmlElement: HTMLElement): OrbitControls {
+ * @param htmlElement - html (canvas?) element on which to listen for mouse events
+ */
+export function setupOrbitControls(
+  camera: Camera,
+  htmlElement: HTMLElement,
+): OrbitControls {
+  const controls = new OrbitControls(camera, htmlElement);
+  controls.autoRotate = false;
+  controls.enableDamping = true;
 
-    const controls = new OrbitControls(camera, htmlElement);
-    controls.autoRotate = false;
-    controls.enableDamping = true;
+  //IF you change the camera transform manually (e.g. position, orientation) you MUST call controls.update() after.
+  // camera.position.set(0, 20, 100);
+  // controls.update();
 
-    //IF you change the camera transform manually (e.g. position, orientation) you MUST call controls.update() after.
-    // camera.position.set(0, 20, 100);
-    // controls.update();
-
-    return controls;
+  return controls;
 }

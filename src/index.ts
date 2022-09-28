d25e980350d24fc5cdaa223a738ff57b7fd05ff4
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-09-28 15:49:56 +0100

    Set up basic scene with loaded demo model

--- src/index.ts
+++ src/index.ts
@@ -1,106 +1,42 @@
-import { Object3D } from 'three';
-import { Scene } from 'three';
-import { dumpObjectToConsoleAsString } from './debugModel';
-import { loadModel } from './loadModel';
-import { setupCamera } from './setupCamera';
-import { setupHelpers } from './setupHelpers';
-import { setupLights } from './setupLights';
-import { setupOrbitControls } from './setupOrbitControls';
-import { setupRenderer } from './setupRenderer';
+import { Scene } from "three";
+import { dumpObjectToConsoleAsString } from "./debugModel";
+import { loadModel } from "./loadModel";
+import { setupCamera } from "./setupCamera";
+import { setupHelpers } from "./setupHelpers";
+import { setupLights } from "./setupLights";
+import { setupOrbitControls } from "./setupOrbitControls";
+import { setupRenderer } from "./setupRenderer";
 
 export async function setupThreeJSScene(): Promise<void> {
+  const dimensions = { w: window.innerWidth, h: window.innerHeight };
 
-    const dimensions = { w: window.innerWidth, h: window.innerHeight };
+  const camera = setupCamera(dimensions);
 
-    const camera = setupCamera(dimensions);
+  const renderer = setupRenderer(camera, dimensions);
 
-    const renderer = setupRenderer(camera, dimensions);
+  const scene = new Scene();
 
-    //Currently, the orbit controls will fight with the automated camera movement in animate()
-    const controls = setupOrbitControls(camera, renderer.domElement);
+  setupLights(scene);
 
-    const scene = new Scene();
+  setupHelpers(scene);
 
+  setupOrbitControls(camera, renderer.domElement);
 
-    setupLights(scene);
+  //Load a model and add it to the scene!
+  const submarine = await loadModel("./assets/lionSubmariners.glb");
+  if (submarine) {
+    scene.add(submarine);
 
-    setupHelpers(scene);
-
-    //Load a model of a submarine and add it to the scene!
-    const submarine = await loadModel("./assets/lionSubmariners.glb");
-    if (submarine) {
-        submarine.scale.set(5, 5, 5);
-        submarine.position.setZ(50);
-        scene.add(submarine);
-
-        //Optional: See in console what the model / scene consists of
-        dumpObjectToConsoleAsString(submarine)
-
-        //Optional: find a subpart of the model and store it in userData (or some other variable)
-        // (for later animation)
-        submarine.traverse(child => {
-            if (child.name === "sub_prop") {
-                submarine.userData.propeller = child;
-            }
-            if (child.name === "sub_periscope") {
-                submarine.userData.periscope = child;
-            }
-        });
-    }
-
-    //You can get more models from https://market.pmnd.rs/
-
-    //keep a frame counter so we can use it as an input to an animation
-    let frameCount = 0;
+    //Optional: See in console what the model / scene consists of
+    dumpObjectToConsoleAsString(submarine);
 
     animate();
 
     function animate() {
-        renderer.render(scene, camera);
-
-        if (submarine) {
-            animateSubmarine(submarine);
-            moveCameraAlongsideSubmarine(submarine);
-            //either /  or move the camera automatically or allow the user to control it        
-            false && controls.update(); // required if controls has .enableDamping .autoRotate set true.
-        }
-
-
-
-        const infoElem = document.getElementById("info");
-        if (infoElem && submarine) {
-            infoElem.innerText = "z: " + Math.round(submarine.position.z);
-        }
-        requestAnimationFrame(animate);
-        frameCount++;
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
     }
-
-    function animateSubmarine(submarine: Object3D) {
-        //moving forward
-        submarine.position.setZ(submarine.position.z -= 0.1)
-        //bobbing up and down with a sine wave
-        submarine.position.setY(Math.sin(frameCount / 20));
-        if (submarine.userData.propeller) {
-            submarine.userData.propeller.rotation.y += 0.1;
-        }
-        if (submarine.userData.periscope) {
-            //correct
-            submarine.userData.periscope.rotation.z = Math.sin(frameCount / 40);
-            //but funnier
-            // submarine.userData.periscope.rotation.y = Math.sin(frameCount / 10);
-        }
-
-    }
-    //unimportant
-    function moveCameraAlongsideSubmarine(submarine: Object3D) {
-        camera.position.copy(submarine.position);
-        camera.position.y = 10;
-        camera.position.x -= 20;
-        camera.position.z += 5 + Math.sin(frameCount / 120) * 20;
-        const lookAtTarget = submarine.position.clone();
-        lookAtTarget.y = 0;
-        camera.lookAt(lookAtTarget)
-    }
+  }
 }
 
 setupThreeJSScene();

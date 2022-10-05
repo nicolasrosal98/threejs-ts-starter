77c7e6a7a67e3258ebdf4857b79f8ab6255c8d58
Nicolas Rosal <nicolasrosal98@gmail.com>
2022-10-05 16:33:27 +0100

    Add decs.d.ts

---
+++ decs.d.ts
@@ -0,0 +1,10 @@
+declare module "three/examples/jsm/libs/lil-gui.module.min.js" {
+  // class GUI {
+  //     add: any;
+  //     addColor: any;
+  //     addFolder: any;
+  // }
+  const GUI: any;
+  type GUI = any;
+  export { GUI };
+}
